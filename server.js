76862279fa9a5e10907eb0917bfa4972b9cc3059
fdirige <118780727+fdirige@users.noreply.github.com>
2023-03-05 12:33:20 -0800

    updated index.html and server.js to reflect /notes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('api/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
     res.json(allNotes.slice(1));
 });
 
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
